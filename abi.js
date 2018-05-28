c0ac7cbbead677141ec0b79b38485aa210238bb7
Pasha Rumkin <dev@rumk.in>
2018-05-28 21:25:08 +0300

    [src] Simplify group contract ABI

--- abi.js
+++ abi.js
@@ -1,20 +1,6 @@
 module.exports = [
   {
     constant: true,
-    inputs: [],
-    name: 'getSenderRank',
-    outputs: [
-      {
-        name: '',
-        type: 'uint8',
-      },
-    ],
-    payable: false,
-    stateMutability: 'view',
-    type: 'function',
-  },
-  {
-    constant: true,
     inputs: [
       {
         name: '_addr',
@@ -30,24 +16,6 @@ module.exports = [
     ],
     payable: false,
     stateMutability: 'view',
-    type: 'function',
-  },
-  {
-    constant: false,
-    inputs: [
-      {
-        name: '_addr',
-        type: 'address',
-      },
-      {
-        name: '_rank',
-        type: 'uint8',
-      },
-    ],
-    name: 'setRank',
-    outputs: [],
-    payable: true,
-    stateMutability: 'payable',
     type: 'function',
   },
 ];
